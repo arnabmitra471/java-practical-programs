0bb3e80a4df15031229a01ad666ec07adb2f0358
Arnab Mitra <arnabmitra471@gmail.com>
2024-01-11 12:13:38 +0530

    Demonstrating the use of a static block

---
+++ Static_Block.java
@@ -0,0 +1,11 @@
+class Static_Block
+{
+    static
+    {
+        System.out.println("Hey !! I am inside a static block");
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Hey I am inside the main method");
+    }
+}
